47e2ee2dda4e72dd26756f9a882200f3e41ba486
Eleftherios Moschos <tmoschos@bigpond.com>
2020-08-11 22:33:10 +0930

    Created file structure and folers

---
+++ config/orm.js
@@ -0,0 +1,6 @@
+/* These are the methods you will need to use in order to retrieve and store data in your database.
+selectAll() 
+insertOne() 
+updateOne()
+Export the ORM object in `module.exports`.
+*/
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE burgers_db;
+
+INSERT INTO burger (burger_name)
+VALUES ("Big Western Burger"),("Double Chicken and Cheese"), ("Smoky Ranch with Chipotle");
+
+SELECT * FROM burger;
